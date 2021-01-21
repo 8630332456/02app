3d317a1865caf244f34fb84af31110b2f8ca660d
= <=>
2021-01-21 11:08:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlighterDirective } from './Highlighterdirective/highlighterdirective.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighlighterDirective,
   ],
   providers: [],
   bootstrap: [AppComponent]
